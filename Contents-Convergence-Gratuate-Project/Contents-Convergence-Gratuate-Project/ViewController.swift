057d7b5c1f060a06e5a7e02b604cd8db15a58cb1
Guel-git <eccyuna@gmail.com>
2023-10-18 23:33:11 +0900

    [ADD] Storyboard 제거

---
+++ Contents-Convergence-Gratuate-Project/Contents-Convergence-Gratuate-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contents-Convergence-Gratuate-Project
+//
+//  Created by 김유나 on 2023/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
