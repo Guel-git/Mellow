4e3f6a88f4dc9c69f09a7cc33ad36b085e58e2fd
Guel-git <eccyuna@gmail.com>
2023-10-18 23:40:55 +0900

    [ADD] 프로젝트 생성

---
+++ Contents-Convergence-Graduate-Project/Contents-Convergence-Graduate-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contents-Convergence-Graduate-Project
+//
+//  Created by 김유나 on 2023/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
