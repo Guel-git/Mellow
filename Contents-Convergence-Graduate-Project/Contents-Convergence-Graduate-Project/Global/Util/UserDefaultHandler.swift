9abc99b233cfee0303682b6a23862de4a79ad141
Guel-git <eccyuna@gmail.com>
2023-10-23 18:47:52 +0900

    [ADD] UserDefault 추가

---
+++ Contents-Convergence-Graduate-Project/Contents-Convergence-Graduate-Project/Global/Util/UserDefaultHandler.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultHandler.swift
+//  Contents-Convergence-Graduate-Project
+//
+//  Created by 김유나 on 2023/10/23.
+//
+
+import Foundation
+
+struct UserDefaultHandler {
+//    @UserDefault(key: "accessToken", defaultValue: "")
+//    static var accessToken: String
+}
