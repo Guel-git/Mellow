56defc6bf6752687b7f886cc6b27f5a3589cab80
Guel-git <eccyuna@gmail.com>
2023-10-23 18:55:53 +0900

    [ADD] Font & Color Extension 추가

---
+++ Contents-Convergence-Graduate-Project/Contents-Convergence-Graduate-Project/Global/Extension/UIFont+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIFont+Extension.swift
+//  Contents-Convergence-Graduate-Project
+//
+//  Created by 김유나 on 2023/10/23.
+//
+
+import UIKit
+
+//enum AppFontName: String {
+//    case regular = "Pretendard-Regular"
+//    case bold = "Pretendard-Bold"
+//    case medium = "Pretendard-Medium"
+//    case semiBold = "Pretendard-SemiBold"
+//}
+
+extension UIFont {
+//    static func font(_ style: AppFontName, ofSize size: CGFloat) -> UIFont {
+//        return UIFont(name: style.rawValue, size: size)!
+//    }
+    
+//    static var h1: UIFont {
+//        return font(.semiBold, ofSize: 24)
+//    }
+}
