76ad8fa5d98e92efd9f39f548c755cebcc68ba71
Guel-git <eccyuna@gmail.com>
2023-10-27 02:15:36 +0900

    [ADD] QuestionVC 재사용 구현

--- Contents-Convergence-Graduate-Project/Contents-Convergence-Graduate-Project/Global/Literal/TextLiteral.swift
+++ Contents-Convergence-Graduate-Project/Contents-Convergence-Graduate-Project/Global/Literal/TextLiteral.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+enum TextLiteral {
+    
+    // MARK: - answer
+    static let answerList = ["배달 앱을 켜고 야식을 시킨다.", "몸에 좋지 않아! 꾹 참는다.", "집에 있는 음식을 찾아 먹는다."]
+}
--- Contents-Convergence-Graduate-Project/Contents-Convergence-Graduate-Project/Screens/Onboarding/Question/QuestionViewController.swift
+++ Contents-Convergence-Graduate-Project/Contents-Convergence-Graduate-Project/Screens/Onboarding/Question/QuestionViewController.swift
@@ -9,9 +9,68 @@ import UIKit
 
 import SnapKit
 
+enum QuestionNum: Int {
+    case first = 1
+    case second = 2
+    case third = 3
+    case fourth = 4
+        
+    var progressRatio: Double {
+        switch self {
+        case .first:
+            return 0.25
+        case .second:
+            return 0.5
+        case .third:
+            return 0.75
+        case .fourth:
+            return 1.0
+        }
+    }
+    
+    var numImage: UIImage {
+        switch self {
+        case .first:
+            return ImageLiteral.questionOneImage
+        case .second:
+            return ImageLiteral.questionTwoImage
+        case .third:
+            return ImageLiteral.questionThreeImage
+        case .fourth:
+            return ImageLiteral.questionFourImage
+        }
+    }
+    
+    var questionText: String {
+        switch self {
+        case .first:
+            return "자기 전에 배고픈 당신은?"
+        case .second:
+            return "자기 전, 핸드폰을 두는 장소는?"
+        case .third:
+            return "배가 고프면 제일 먼저 먹는 음식은?"
+        case .fourth:
+            return "나는 누구인가?"
+        }
+    }
+    
+    var answerList: [String] {
+        switch self {
+        case .first:
+            return TextLiteral.answerList
+        case .second:
+            return TextLiteral.answerList
+        case .third:
+            return TextLiteral.answerList
+        case .fourth:
+            return TextLiteral.answerList
+        }
+    }
+}
+
 final class QuestionViewController: BaseViewController {
     
-    private let answerList = ["배달 앱을 켜고 야식을 시킨다.", "몸에 좋지 않아! 꾹 참는다.", "집에 있는 음식을 찾아 먹는다."]
+    var questionNum: QuestionNum
     
     // MARK: - property
 
@@ -37,13 +96,11 @@ final class QuestionViewController: BaseViewController {
     }()
     private let questionNumImage: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = ImageLiteral.questionOneImage
         imageView.tintColor = .fontBlack
         return imageView
     }()
     private let questionLabel: UILabel = {
         let label = UILabel()
-        label.text = "자기 전에 배고픈 당신은?"
         label.textColor = .fontBlack
         label.font = .sb20
         return label
@@ -58,10 +115,18 @@ final class QuestionViewController: BaseViewController {
 
     // MARK: - life cycle
     
+    init(questionNum: QuestionNum) {
+        self.questionNum = questionNum
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupDelegate()
         setupAttribute()
+        setButtonAction()
     }
     
     override func render() {
@@ -83,7 +148,7 @@ final class QuestionViewController: BaseViewController {
         
         progressBar.snp.makeConstraints {
             $0.leading.top.bottom.equalToSuperview()
-            $0.width.equalTo(progressBackground.snp.width).multipliedBy(0.25)
+            $0.width.equalTo(progressBackground.snp.width).multipliedBy(questionNum.progressRatio)
         }
         
         questionNumImage.snp.makeConstraints {
@@ -110,33 +175,53 @@ final class QuestionViewController: BaseViewController {
         }
     }
     
+    // MARK: - func
+    
     private func setupDelegate() {
         answerTableView.delegate = self
         answerTableView.dataSource = self
     }
     
     private func setupAttribute() {
+        questionNumImage.image = questionNum.numImage
+        questionLabel.text = questionNum.questionText
+        
         answerTableView.register(AnswerTableViewCell.self, forCellReuseIdentifier: AnswerTableViewCell.cellId)
         answerTableView.rowHeight = 60
         answerTableView.separatorStyle = .none
         answerTableView.isScrollEnabled = false
     }
     
+    private func setButtonAction() {
+        let action = UIAction { [weak self] _ in
+            self?.navigateToNextQuestion()
+        }
+        mainButton.addAction(action, for: .touchUpInside)
+    }
     
-    // MARK: - func
+    private func navigateToNextQuestion() {
+        if questionNum.rawValue == 4 {
+            print("마지막 페이지!")
+        } else {
+            let nextNum = questionNum.rawValue + 1
+            let questionViewController = QuestionViewController(questionNum: QuestionNum(rawValue: nextNum)!)
+            questionViewController.navigationItem.hidesBackButton = true
+            self.navigationController?.pushViewController(questionViewController, animated: true)
+        }
+    }
 }
 
 // MARK: - extension
 
 extension QuestionViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return answerList.count
+        return questionNum.answerList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = answerTableView.dequeueReusableCell(withIdentifier: AnswerTableViewCell.cellId, for: indexPath) as! AnswerTableViewCell
         
-        cell.cellLabel.text = answerList[indexPath.row]
+        cell.cellLabel.text = questionNum.answerList[indexPath.row]
         cell.selectionStyle = .none
         
         return cell
